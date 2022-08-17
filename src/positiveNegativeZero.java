73a3501157b892bfe88562b353ef2eaf78125df5
bibek <chingapinga2@gmail.com>
2022-08-17 21:05:56 +0200

    Java Basics-17th Aug_16:45

--- src/positiveNegativeZero.java
+++ src/positiveNegativeZero.java
@@ -1,10 +1,9 @@
 public class positiveNegativeZero {
 
     public static void checkNumber(int number){
-        System.out.println( (number > 0)? "Positive": (number < 0)? "Negative": "Zero"  );
+        System.out.println( (number > 0)? "Positive": (number < 0)? "Negative": "zero"  );
 
     }
-
     public static void main(String[] args) {
         checkNumber(15);
         checkNumber(-50);
